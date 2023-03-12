125c3cd35ed65c0c2501f39a71ece5ae3a7a4800
MXuanHa <maixuanhathm@gmail.com>
2023-03-13 00:19:37 +0700

    read_map-print_map

---
+++ cau1/problem.py
@@ -0,0 +1,41 @@
+import os
+import numpy
+
+class SingleFoodSearchProblem:
+    def __init__(self):
+        self.matrix = list()
+        self.P = tuple()
+        self.F = dict()
+
+    def __str__(self):
+        res = 'Pacman: '+str(self.P)+"\n"
+        res += 'Food: '+str(self.F)+"\n"
+        for x in self.matrix:
+            temp = ''
+            for y in x:
+                temp += y
+            temp += '\n'
+            res += temp
+        return res
+
+    def print(self):
+        print(str(self))
+
+    def load_from_file(self, filename):
+        if os.path.exists(filename):
+            with open(filename) as g:
+                temp_map = []
+                allmap = g.read().split('\n')
+                row = 0
+                for line in allmap:
+                    arr = [x for x in line]
+                    if '.' in arr:
+                        self.F.update({len(self.F)+1: (row, arr.index('.'))})
+                    if 'P' in arr:
+                        self.P = (row, arr.index('P'))
+                    temp_map.append(arr)
+                    row += 1
+                self.matrix = numpy.array(temp_map)
+g = SingleFoodSearchProblem()
+g.load_from_file('sample_inputs/pacman_single01.txt')
+g.print()
