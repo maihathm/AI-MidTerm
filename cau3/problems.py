f30232a8a26fd95fb8d1a2e280489af6ea32d6a9
S52100628 <52100628@student.tdtu.edu.vn>
2023-03-14 02:32:49 +0700

    update_cau3_yc1

--- cau3/problems.py
+++ cau3/problems.py
@@ -16,6 +16,14 @@ class EightQueenProblem:
             res += temp
         return res
 
+    def h(self):
+        q_list = [self.Q[i] for i in range(len(self.Q))]
+        s= 0
+        for i in q_list:
+            for j in q_list:
+                if ((i[0] - j[0]) == (i[1]- j[1]) and (i[0] - j[0] !=0) and (i[1] - j[1] !=0)) or ((i[0] - j[0]) == -(i[1] - j[1]) and (i[0] - j[0] !=0) and (i[1] - j[1] !=0)) or (i[0] == j[0] and i[1] != j[1]) or (i[0] != j[0] and i[1] == j[1]):
+                    s+=1
+        print(int(s/2))
     def print(self):
         print(str(self))
 
@@ -33,7 +41,9 @@ class EightQueenProblem:
                                 self.Q.update({len(self.Q): (row,i)})
                     temp_map.append(arr)
                     row += 1
-                self.matrix = numpy.array(temp_map)
+                self.matrix = numpy.array(temp_map)  
+
 g = EightQueenProblem()
 g.load_from_file('sample_inputs/eight_queens03.txt')
 g.print()
+g.h()
