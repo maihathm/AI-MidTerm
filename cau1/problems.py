872eb2ba80480d8989cb9cf1645bdb4469faaaf1
MXuanHa <maixuanhathm@gmail.com>
2023-03-18 20:39:50 +0700

    update

--- cau1/problems.py
+++ cau1/problems.py
@@ -1,3 +1,21 @@
+import os
+import numpy
+class SingleFoodSearchProblem:
+    def __init__(self):
+        self.matrix = list()
+        self.P = tuple()
+        self.F = dict()
+    def __str__(self):
+        res = ''
+        for x in self.matrix:
+            temp = ''
+            for y in x:
+                temp += y
+            temp += '\n'
+            res += temp
+        return res
+    def print(self):
+        print(str(self))
 
     def load_from_file(self, filename):
         if os.path.exists(filename):
@@ -14,10 +32,22 @@
                     temp_map.append(arr)
                     row += 1
                 self.matrix = numpy.array(temp_map)
-    def getSuccessor(self,state):
+    def get_successor(self,state):
         successor=[]
         row,col=state
         new_state=tuple()
+        if self.matrix[row-1][col]!='%':
+            new_state=(row-1, col)
+            successor.append((new_state,'N'))
+        if self.matrix[row+1][col]!='%':
+            new_state=( row+1, col)
+            successor.append((new_state,'S'))
+        if self.matrix[row][col-1]!='%':
+            new_state=(row, col-1)
+            successor.append((new_state,"W"))
+        if self.matrix[row][col+1]!='%':
+            new_state=(row,col+1)
+            successor.append((new_state,'E'))
         if row < len(self.matrix) and col<len(self.matrix[row]):
             if row-1>=0:
                 if self.matrix[row-1][col]!='%':
@@ -35,8 +65,6 @@
                 if self.matrix[row][col+1]!='%':
                     new_state=(row,col+1)
                     successor.append((new_state,'E'))
-        # if self.matrix[row][col]=='.':
-        #     actions.append('Stop')
         return successor
     def path_cost(self,cost):
         return cost+1
