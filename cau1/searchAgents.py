857abb050b26254a71e9f7ff25a91080795ec5b7
MXuanHa <maixuanhathm@gmail.com>
2023-03-19 00:12:03 +0700

    cau2

--- cau1/searchAgents.py
+++ cau1/searchAgents.py
@@ -2,14 +2,14 @@ import fringes
 from problems import SingleFoodSearchProblem,MultiFoodSearchProblem
 import copy
 class searchAgents:
-    def EuclideanHeuristic(state,goal_state:dict):
+    def EuclideanHeuristic(seft,state,goal_state:dict):
         row_s,col_s=[int(i) for i in state]
         heuristic=goal_state.copy()
         for i in goal_state:
             row_g,col_g=[int(j) for j in goal_state[i]]
             heuristic[i]=((row_g-row_s)**2+(col_g-col_s)**2)**0.5
-        return heuristic
-    def ManhattanHeuristic(state,goal_state:dict):
+        return min(heuristic.values())
+    def ManhattanHeuristic(seft,state,goal_state:dict):
         row_s,col_s=[int(i) for i in state]
         heuristic=goal_state.copy()
         for i in goal_state:
@@ -39,7 +39,7 @@ class searchAgents:
                     visited.append(new_state)
                     queue.enQueue((new_state,path+[action]))
             print(queue.size())
-        return path
+        return []
     def DFS(self,g:SingleFoodSearchProblem or MultiFoodSearchProblem) -> list:
         m=copy.deepcopy(g)
         pacman_state=m.P
@@ -62,7 +62,7 @@ class searchAgents:
                 if new_state not in visited:
                     visited.append(new_state)
                     stack.push((new_state,path+[action]))
-        return path
+        return []
     def UCS(self,g:SingleFoodSearchProblem or MultiFoodSearchProblem) -> list:
         m=copy.deepcopy(g)
         pacman_state=m.P
@@ -86,13 +86,66 @@ class searchAgents:
                     visited.append(new_state)
                     newpath=path+[action]
                     PriorityQueue.enQueue((new_state,newpath,m.path_cost(cost)))
-        return path
+        return []
+    def astar(self,g:SingleFoodSearchProblem or MultiFoodSearchProblem) -> list:
+        m=copy.deepcopy(g)
+        pacman_state=m.P
+        PriorityQueue=fringes.PriorityQueue(key=lambda x:x[2])
+        PriorityQueue.enQueue((pacman_state,[],0))
+        visited=[]
+        while(PriorityQueue.empty()!=True):
+            state,path,cost=PriorityQueue.deQueue()
+            heuristic=self.EuclideanHeuristic(state,m.F)
+            if m.goal_test(state)==True:
+                if len(m.F)<=1:
+                    return path+['Stop']
+                for i in m.F.copy():
+                    if m.F[i]==state:
+                        m.F.pop(i)
+                        visited.clear()
+                        PriorityQueue.clear()
+                        PriorityQueue.enQueue((state,path,m.path_cost(cost)+heuristic))
+                        break
+            for new_state, action in m.get_successor(state):
+                heuristic=self.EuclideanHeuristic(new_state,m.F)
+                if new_state not in visited:
+                    visited.append(new_state)
+                    newpath=path+[action]
+                    PriorityQueue.enQueue((new_state,newpath,m.path_cost(cost)+heuristic))
+        return []
+    def gbfs(self,g:SingleFoodSearchProblem or MultiFoodSearchProblem) -> list:
+        m=copy.deepcopy(g)
+        pacman_state=m.P
+        PriorityQueue=fringes.PriorityQueue(key=lambda x:x[2])
+        PriorityQueue.enQueue((pacman_state,[],0,0))
+        visited=[]
+        while(PriorityQueue.empty()!=True):
+            state,path,weight,cost=PriorityQueue.deQueue()
+            heuristic=self.EuclideanHeuristic(state,m.F)
+            if m.goal_test(state)==True:
+                if len(m.F)<=1:
+                    return path+['Stop']
+                for i in m.F.copy():
+                    if m.F[i]==state:
+                        m.F.pop(i)
+                        visited.clear()
+                        PriorityQueue.clear()
+                        PriorityQueue.enQueue((state,path,cost+heuristic,cost))
+                        break
+            for new_state, action in m.get_successor(state):
+                heuristic=self.EuclideanHeuristic(new_state,m.F)
+                if new_state not in visited:
+                    visited.append(new_state)
+                    newpath=path+[action]
+                    cost=m.path_cost(cost)
+                    PriorityQueue.enQueue((new_state,newpath,cost+heuristic,cost))
+        return []
 SFSP=SingleFoodSearchProblem()
 MFSP=MultiFoodSearchProblem()
 searcher=searchAgents()
 def algorithm(problems:SingleFoodSearchProblem or MultiFoodSearchProblem):
     while True:
-        number=input("algorithm:\n1.BFS\t2.DFS\t3.UCS\n")
+        number=input("algorithm:\n1.BFS\t2.DFS\t3.UCS\t4.AStar\t5.GBFS\n")
         number=int(number)
         if number==1:
             path=searcher.BFS(problems)
@@ -100,6 +153,10 @@ def algorithm(problems:SingleFoodSearchProblem or MultiFoodSearchProblem):
             path=searcher.DFS(problems)
         elif number==3:
             path=searcher.UCS(problems)
+        elif number==4:
+            path=searcher.astar(problems)
+        elif number==5:
+            path=searcher.gbfs(problems)
         else:
             break
         problems.animate(path)
