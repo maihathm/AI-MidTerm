539832948f90a30cc446708df1fa7280b2541bc5
S52100898 <52100898@student.tdtu.edu.vn>
2023-03-19 23:52:11 +0700

    tong hop file

---
+++ source/fringes.py
@@ -0,0 +1,56 @@
+class __List:
+    def __init__(self) -> None:
+        self._list = []
+
+    def size(self) -> int:
+        return len(self._list)
+
+    def empty(self) -> bool:
+        return len(self._list) == 0
+
+    def contain(self, item) -> bool:
+        return item in self._list
+
+    def clear(self) -> None:
+        self._list.clear()
+
+    def __str__(self) -> str:
+        return str(self._list)
+
+
+class Stack(__List):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def push(self, item):
+        self._list.append(item)
+
+    def pop(self):
+        return self._list.pop()
+
+    def peek(self):
+        return self._list[-1]
+
+
+class Queue(__List):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def en_queue(self, item):
+        self._list.append(item)
+
+    def de_queue(self):
+        return self._list.pop(0)
+
+    def peek(self):
+        return self._list[0]
+
+
+class PriorityQueue(Queue):
+    def __init__(self, key=None) -> None:
+        super().__init__()
+        self.__key = key
+
+    def en_queue(self, item):
+        super().en_queue(item)
+        self._list.sort(key=self.__key)
---
+++ source/test.py
@@ -0,0 +1,104 @@
+from problems import SingleFoodSearchProblem, MultiFoodSearchProblem, EightQueenProblem
+from searchAgents import bfs, dfs, ucs, astar, gbfs, Euclidean_heuristic, Manhattan_heuristic
+
+
+def cau1_2():
+    while True:
+        number = int(input(
+            "Testcase:\n1.Single01\t2.Single02\t3.Single03\t4.Multi01\t5.Multi02\t6.Multi03\n"))
+        if number == 1:
+            problem = SingleFoodSearchProblem()
+            problem.load_from_file("sample_inputs/pacman_single01.txt")
+            algorithm1_2(problem)
+        if number == 2:
+            problem = SingleFoodSearchProblem()
+            problem.load_from_file("sample_inputs/pacman_single02.txt")
+            algorithm1_2(problem)
+        if number == 3:
+            problem = SingleFoodSearchProblem()
+            problem.load_from_file("sample_inputs/pacman_single03.txt")
+            algorithm1_2(problem)
+        if number == 4:
+            problem = MultiFoodSearchProblem()
+            problem.load_from_file("sample_inputs/pacman_multi01.txt")
+            algorithm1_2(problem)
+        if number == 5:
+            problem = MultiFoodSearchProblem()
+            problem.load_from_file("sample_inputs/pacman_multi02.txt")
+            algorithm1_2(problem)
+        if number == 6:
+            problem = MultiFoodSearchProblem()
+            problem.load_from_file("sample_inputs/pacman_multi03.txt")
+            algorithm1_2(problem)
+        else:
+            break
+
+
+def algorithm1_2(problem: SingleFoodSearchProblem or MultiFoodSearchProblem):
+    while True:
+        number = int(
+            input("Algorithm:\n1.BFS\t2.DFS\t3.UCS\t4.AStar\t5.GBFS\n"))
+        if number == 1:
+            path = bfs(problem)
+            problem.animate(path)
+        elif number == 2:
+            path = dfs(problem)
+            problem.animate(path)
+        elif number == 3:
+            path = ucs(problem)
+            problem.animate(path)
+        elif number == 4:
+            heuristic = choose_heuristic()
+            if heuristic == 1:
+                path = astar(problem, Euclidean_heuristic)
+                problem.animate(path)
+            elif heuristic == 2:
+                path = astar(problem, Manhattan_heuristic)
+                problem.animate(path)
+        elif number == 5:
+            heuristic = choose_heuristic()
+            if heuristic == 1:
+                path = gbfs(problem, Euclidean_heuristic)
+                problem.animate(path)
+            elif heuristic == 2:
+                path = gbfs(problem, Manhattan_heuristic)
+                problem.animate(path)
+        else:
+            break
+
+
+def choose_heuristic():
+    while True:
+        number = int(input("Heuristic:\n1.Euclidean\t2.Manhattan\n"))
+        if number == 1 or number == 2:
+            return number
+
+
+def cau3():
+    while True:
+        number = int(input("Testcase:\n1.Queen01\t2.Queen02\t3.Queen03\n"))
+        if number == 1:
+            problem = EightQueenProblem()
+            problem.load_from_file("sample_inputs/eight_queens01.txt")
+            algorithm3(problem)
+        if number == 2:
+            problem = EightQueenProblem()
+            problem.load_from_file("sample_inputs/eight_queens02.txt")
+            algorithm3(problem)
+        if number == 3:
+            problem = EightQueenProblem()
+            problem.load_from_file("sample_inputs/eight_queens03.txt")
+            algorithm3(problem)
+        else:
+            break
+
+def algorithm3(problem):
+    problem.print()
+    problem.__newinit__()
+    print(problem.h([0, 7]))
+    problem.print_board()
+
+
+
+#cau1_2()
+#cau3()
