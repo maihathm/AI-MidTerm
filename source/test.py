1b37307f121cbdd49db9f8a5b41fc15d7de0fd5a
S52100898 <52100898@student.tdtu.edu.vn>
2023-03-20 15:23:46 +0700

    final_version_test.py

--- source/test.py
+++ source/test.py
@@ -4,66 +4,70 @@ from searchAgents import bfs, dfs, ucs, astar, gbfs, Euclidean_heuristic, Manhat
 
 def cau1_2():
     while True:
-        number = int(input(
-            "Testcase:\n1.Single01\t2.Single02\t3.Single03\t4.Multi01\t5.Multi02\t6.Multi03\n"))
-        if number == 1:
-            problem = SingleFoodSearchProblem()
-            problem.load_from_file("sample_inputs/pacman_single01.txt")
-            algorithm1_2(problem)
-        elif number == 2:
-            problem = SingleFoodSearchProblem()
-            problem.load_from_file("sample_inputs/pacman_single02.txt")
-            algorithm1_2(problem)
-        elif number == 3:
-            problem = SingleFoodSearchProblem()
-            problem.load_from_file("sample_inputs/pacman_single03.txt")
-            algorithm1_2(problem)
-        elif number == 4:
-            problem = MultiFoodSearchProblem()
-            problem.load_from_file("sample_inputs/pacman_multi01.txt")
-            algorithm1_2(problem)
-        elif number == 5:
-            problem = MultiFoodSearchProblem()
-            problem.load_from_file("sample_inputs/pacman_multi02.txt")
-            algorithm1_2(problem)
-        elif number == 6:
-            problem = MultiFoodSearchProblem()
-            problem.load_from_file("sample_inputs/pacman_multi03.txt")
-            algorithm1_2(problem)
-        else:
+        try:
+            number = int(input("Testcase:\n1.Single01\t2.Single02\t3.Single03\t4.Multi01\t5.Multi02\t6.Multi03\n"))
+            if number == 1:
+                problem = SingleFoodSearchProblem()
+                problem.load_from_file("../sample_inputs/pacman_single01.txt")
+                algorithm1_2(problem)
+            elif number == 2:
+                problem = SingleFoodSearchProblem()
+                problem.load_from_file("../sample_inputs/pacman_single02.txt")
+                algorithm1_2(problem)
+            elif number == 3:
+                problem = SingleFoodSearchProblem()
+                problem.load_from_file("../sample_inputs/pacman_single03.txt")
+                algorithm1_2(problem)
+            elif number == 4:
+                problem = MultiFoodSearchProblem()
+                problem.load_from_file("../sample_inputs/pacman_multi01.txt")
+                algorithm1_2(problem)
+            elif number == 5:
+                problem = MultiFoodSearchProblem()
+                problem.load_from_file("../sample_inputs/pacman_multi02.txt")
+                algorithm1_2(problem)
+            elif number == 6:
+                problem = MultiFoodSearchProblem()
+                problem.load_from_file("../sample_inputs/pacman_multi03.txt")
+                algorithm1_2(problem)
+            else:
+                break
+        except:
             break
 
 
 def algorithm1_2(problem: SingleFoodSearchProblem or MultiFoodSearchProblem):
     while True:
-        number = int(
-            input("Algorithm:\n1.BFS\t2.DFS\t3.UCS\t4.AStar\t5.GBFS\n"))
-        if number == 1:
-            path = bfs(problem)
-            print(path)
-        elif number == 2:
-            path = dfs(problem)
-            print(path)
-        elif number == 3:
-            path = ucs(problem)
-            print(path)
-        elif number == 4:
-            heuristic = choose_heuristic()
-            if heuristic == 1:
-                path = astar(problem, Euclidean_heuristic)
+        try:
+            number = int(input("Algorithm:\n1.BFS\t2.DFS\t3.UCS\t4.AStar\t5.GBFS\n"))
+            if number == 1:
+                path = bfs(problem)
                 print(path)
-            elif heuristic == 2:
-                path = astar(problem, Manhattan_heuristic)
+            elif number == 2:
+                path = dfs(problem)
                 print(path)
-        elif number == 5:
-            heuristic = choose_heuristic()
-            if heuristic == 1:
-                path = gbfs(problem, Euclidean_heuristic)
+            elif number == 3:
+                path = ucs(problem)
                 print(path)
-            elif heuristic == 2:
-                path = gbfs(problem, Manhattan_heuristic)
-                print(path)
-        else:
+            elif number == 4:
+                heuristic = choose_heuristic()
+                if heuristic == 1:
+                    path = astar(problem, Euclidean_heuristic)
+                    print(path)
+                elif heuristic == 2:
+                    path = astar(problem, Manhattan_heuristic)
+                    print(path)
+            elif number == 5:
+                heuristic = choose_heuristic()
+                if heuristic == 1:
+                    path = gbfs(problem, Euclidean_heuristic)
+                    print(path)
+                elif heuristic == 2:
+                    path = gbfs(problem, Manhattan_heuristic)
+                    print(path)
+            else:
+                break
+        except:
             break
 
 
@@ -76,20 +80,23 @@ def choose_heuristic():
 
 def cau3():
     while True:
-        number = int(input("Testcase:\n1.Queen01\t2.Queen02\t3.Queen03\n"))
-        if number == 1:
-            problem = EightQueenProblem()
-            problem.load_from_file("sample_inputs/eight_queens01.txt")
-            algorithm3(problem)
-        elif number == 2:
-            problem = EightQueenProblem()
-            problem.load_from_file("sample_inputs/eight_queens02.txt")
-            algorithm3(problem)
-        elif number == 3:
-            problem = EightQueenProblem()
-            problem.load_from_file("sample_inputs/eight_queens03.txt")
-            algorithm3(problem)
-        else:
+        try:
+            number = int(input("Testcase:\n1.Queen01\t2.Queen02\t3.Queen03\n"))
+            if number == 1:
+                problem = EightQueenProblem()
+                problem.load_from_file("../sample_inputs/eight_queens01.txt")
+                algorithm3(problem)
+            elif number == 2:
+                problem = EightQueenProblem()
+                problem.load_from_file("../sample_inputs/eight_queens02.txt")
+                algorithm3(problem)
+            elif number == 3:
+                problem = EightQueenProblem()
+                problem.load_from_file("../sample_inputs/eight_queens03.txt")
+                algorithm3(problem)
+            else:
+                break
+        except:
             break
 
 def algorithm3(problem):
@@ -100,5 +107,5 @@ def algorithm3(problem):
 
 
 
-# cau1_2()
+#cau1_2()
 #cau3()
